d2d8c0a6fd8e8f90e8c0318cc637928f5ba358c1
Jaki Chen <JakiChen@live.cn>
2020-05-08 13:55:44 +0800

    update

--- js/switch.js
+++ js/switch.js
@@ -1,1 +1,11 @@
-$(function(){var a=navigator.userAgent;-1!=a.search(/iPhone/)||-1!=a.search(/iPad/)||-1!=a.search(/iPod/)||-1!=a.search(/Android/)?$.ajax({url:"/js/app_sp.js?190505",dataType:"script",cache:!1}):$.ajax({url:"/js/app.js?190505",dataType:"script",cache:!1})});
+$(function () {
+    var a = navigator.userAgent; - 1 != a.search(/iPhone/) || -1 != a.search(/iPad/) || -1 != a.search(/iPod/) || -1 != a.search(/Android/) ? $.ajax({
+        url: "/js/app_sp.js?190505",
+        dataType: "script",
+        cache: !1
+    }) : $.ajax({
+        url: "/js/app.js?190505",
+        dataType: "script",
+        cache: !1
+    })
+});
