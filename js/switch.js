f248eb70e428e53236d3fa481e0e6c9088be3c59
Jaki Chen <JakiChen@live.cn>
2020-05-08 13:11:20 +0800

    upload

---
+++ js/switch.js
@@ -0,0 +1,1 @@
+$(function(){var a=navigator.userAgent;-1!=a.search(/iPhone/)||-1!=a.search(/iPad/)||-1!=a.search(/iPod/)||-1!=a.search(/Android/)?$.ajax({url:"/js/app_sp.js?190505",dataType:"script",cache:!1}):$.ajax({url:"/js/app.js?190505",dataType:"script",cache:!1})});
